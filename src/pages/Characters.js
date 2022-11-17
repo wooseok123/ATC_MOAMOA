ba2fe6b5675ef08227e661b40ec17436e2273803
wooseok123 <hosmimam@naver.com>
2022-11-17 13:31:26 +0900

    done

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -5,6 +5,8 @@ import Meow from "../imgs/Meow.png";
 import Azae from "../imgs/Azae.png";
 import cryingKid from "../imgs/Crying_kid.png";
 import Gurumi from "../imgs/Gurumi.png";
+import blackDesc from "../imgs/blackboots_desc.png";
+import DrdrDesc from "../imgs/Drdr_desc.png";
 
 const Characters = () => {
   const slideEffects = useRef([]);
@@ -19,7 +21,7 @@ const Characters = () => {
   const slideUp = () => {
     slideEffects.current.forEach((el) => {
       const currentEl = el.getBoundingClientRect();
-      if (currentEl.top < 600) {
+      if (currentEl.top < 700) {
         if (el.classList[1]) return;
 
         if (el.id == "blackboots") {
@@ -35,23 +37,49 @@ const Characters = () => {
 
   return (
     <div>
-      <h2>Characters</h2>
+      <h2 style={{ fontSize: "64px", marginBottom: "40px" }}>Characters</h2>
       <div className="character__main">
-        <div className="characters">
-          <img
-            ref={(el) => (slideEffects.current[0] = el)}
-            src={BlackBoots}
-            id="blackboots"
-            className="character"
-          ></img>
+        <div
+          ref={(el) => (slideEffects.current[0] = el)}
+          id="blackboots"
+          className="character"
+        >
+          <img src={BlackBoots}></img>
+          <div>
+            <img src={blackDesc}></img>
+          </div>
+          <p
+            style={{
+              position: "absolute",
+              bottom: "20px",
+              right: "0px",
+              transform: "rotate(-15deg)",
+              fontSize: "30px",
+            }}
+          >
+            Our Main Character!
+          </p>
         </div>
-        <div className="characters">
-          <img
-            id="Drdr"
-            ref={(el) => (slideEffects.current[1] = el)}
-            src={Drdr}
-            className="character"
-          ></img>
+        <div
+          className="character"
+          id="Drdr"
+          ref={(el) => (slideEffects.current[1] = el)}
+        >
+          <div>
+            <img src={DrdrDesc}></img>
+          </div>
+          <img src={Drdr}></img>
+          <p
+            style={{
+              position: "absolute",
+              bottom: "20px",
+              left: "0px",
+              transform: "rotate(20deg)",
+              fontSize: "30px",
+            }}
+          >
+            Our Genius Inventor!
+          </p>
         </div>
       </div>
       <div className="character__sub">
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,6 +5,7 @@ import MainFront1 from "../imgs/MainFront1.png";
 import MainFront2 from "../imgs/MainFront2.png";
 import MainCloud1 from "../imgs/MainCloud1.png";
 import MainCloud2 from "../imgs/MainCloud2.png";
+import logo from "../imgs/logo.png";
 import walkingBoots from "../imgs/blackBoots_walking.gif";
 import { Main1Cloud, Main2Cloud } from "../styledComponents";
 
@@ -13,6 +14,9 @@ import MainBgImg from "../components/MainBgImg";
 const Main = () => {
   return (
     <div className="Main__Wrapper">
+      <div style={{ zIndex: 10, position: "absolute", top: "150px" }}>
+        <img src={logo}></img>
+      </div>
       <div className="MainCloud__Wrapper">
         <Main1Cloud src={MainCloud1}></Main1Cloud>
         <Main2Cloud src={MainCloud2}></Main2Cloud>
@@ -22,7 +26,7 @@ const Main = () => {
         <MainBgImg src={MainBack2} />
         <MainBgImg src={MainFront1} />
         <MainBgImg src={MainFront2} />
-        <img style={{ width: "100px", zIndex: 6 }} src={walkingBoots}></img>
+        <img style={{ width: "200px", zIndex: 6 }} src={walkingBoots}></img>
         <div
           style={{
             zIndex: "5",
@@ -30,6 +34,7 @@ const Main = () => {
             padding: "20px",
             borderRadius: "10px",
             marginBottom: "100px",
+            fontFamily: "Black Han Sans",
           }}
         >
           Press To Start
