f8d556c0ed9a505cf1b13fb9b849931fa42c0fc4
wooseok123 <hosmimam@naver.com>
2022-11-20 22:50:14 +0900

    하이

--- src/pages/Cartoon.js
+++ src/pages/Cartoon.js
@@ -61,7 +61,7 @@ const Cartoon = () => {
             zIndex: 100,
             position: "fixed",
             top: "0px",
-            width: "420px",
+            width: "380px",
             height: "100vh",
             backgroundColor: "rgba(0,0,0,0.5)",
           }}
